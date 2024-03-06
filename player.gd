9142f0cb32fc82478cd84888a600ae575b78121e
jonahh2160 <144245615+jonahh2160@users.noreply.github.com>
2024-03-06 17:56:21 -0600

    HP and i-frames for player

--- player.gd
+++ player.gd
@@ -1,8 +1,10 @@
 extends Area2D
+signal hit
 
 # Called when the node enters the scene tree for the first time.
 @export var speed = 400
-var health = 3
+@export var health = 3
+var i_frames = 0
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
@@ -26,3 +28,19 @@ func _process(delta):
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+func _on_body_entered(body):
+	if i_frames == 0:
+		health -= 1
+		i_frames += 150
+		$DamageSound.play()
+	if health == 0:
+		hide() # Player disappears after being hit.
+		hit.emit()
+		# Must be deferred as we can't change physics properties on a physics callback.
+		$CollisionShape2D.set_deferred("disabled", true) # Don't want player to get hit twice
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
