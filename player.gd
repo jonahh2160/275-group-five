51fbbf6c2e1301eb721d0acdf0948bd7ad5ee1b6
jonahh2160 <144245615+jonahh2160@users.noreply.github.com>
2024-03-07 13:37:11 -0600

    Improved player movement greatly

--- player.gd
+++ player.gd
@@ -1,46 +1,49 @@
 extends Area2D
 signal hit
 
-# Called when the node enters the scene tree for the first time.
-@export var speed = 400
-@export var health = 3
+@export var max_vel = 400
+@export var accel = 1500
+@export var decel = 600
+
+var velocity = Vector2()
+var dir = Vector2.ZERO
+var state = 0
 var i_frames = 0
 var screen_size
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+	match state:
+		0:
+			movement(delta)
+		1:
+			dodge(delta)
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 
-func _on_body_entered(body):
-	if i_frames == 0:
-		health -= 1
-		i_frames += 150
-		$DamageSound.play()
-	if health == 0:
-		hide() # Player disappears after being hit.
-		hit.emit()
-		# Must be deferred as we can't change physics properties on a physics callback.
-		$CollisionShape2D.set_deferred("disabled", true) # Don't want player to get hit twice
+func get_dir():
+	dir.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	dir.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+	return dir.normalized()
 
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
+func movement(delta):
+	dir = get_dir()
+	
+	if dir == Vector2.ZERO:
+		if velocity.length() > (decel * delta):
+			velocity -= velocity.normalized() * (decel * delta)
+		else:
+			velocity = Vector2.ZERO
+	else:
+		velocity += (dir * accel * delta)
+		velocity = velocity.limit_length(max_vel)
+
+func dodge(delta):
+	pass
