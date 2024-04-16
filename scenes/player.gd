a7b82affe535e4c4fead7b4ef533d706ce734f86
jonahh2160 <144245615+jonahh2160@users.noreply.github.com>
2024-04-16 11:25:28 -0500

    Added a small damage flash and moved functions

--- scenes/player.gd
+++ scenes/player.gd
@@ -15,13 +15,13 @@ var screen_size
 var dashing
 var dash_angle
 
-func get_dir():
-	dir.x = Input.get_axis("move_left", "move_right")
-	dir.y = Input.get_axis("move_up", "move_down")
-	return dir.normalized()
-
 func _ready():
 	screen_size = get_viewport_rect().size
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
 
 func _physics_process(delta):
 	dir = get_dir()
@@ -32,14 +32,20 @@ func _physics_process(delta):
 		1:
 			dodge(delta)
 	
+	# I-Frame reduction + flash
 	if i_frames > 0:
 		i_frames = i_frames - 1
+		
+	if i_frames > 0 and is_visible() and not dashing:
+		damage_flash()
 		
 	move_and_slide()
 
 func free_state(delta):
+	# Dodge
 	if (Input.is_action_pressed("dodge") and dir):
 		state = 1
+		
 	# Credit to KobeDev on YouTube
 	velocity = lerp(velocity, dir * speed, delta * accel)
 
@@ -76,11 +82,18 @@ func dodge(delta):
 	if dashing:
 		velocity += (dash_angle * speed * 2 * delta)	
 
+func get_dir():
+	dir.x = Input.get_axis("move_left", "move_right")
+	dir.y = Input.get_axis("move_up", "move_down")
+	return dir.normalized()
 
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
+func damage_flash():
+	if not is_visible():
+		return
+	else:
+		hide()
+		await get_tree().create_timer(0.02).timeout # wait for 1 second
+		show()
 
 func _on_area_2d_body_entered(body):
 	if i_frames <= 0:
