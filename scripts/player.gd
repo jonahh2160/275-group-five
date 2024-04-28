8d4c8d2b3c726161701cc091b7cfd2a53a6e2507
jonahh2160 <144245615+jonahh2160@users.noreply.github.com>
2024-04-28 17:58:47 -0500

    Small changes to player script

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,8 +12,10 @@ var i_frames = 0
 var phase = 0
 var timer = 0
 var screen_size
-var dashing
 var dash_angle
+var dashing
+var timer_on = false
+
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -35,10 +37,9 @@ func _physics_process(delta):
 	# I-Frame reduction + flash
 	if i_frames > 0:
 		i_frames = i_frames - 1
-		
-	if i_frames > 0 and is_visible() and not dashing:
-		damage_flash()
-		
+		if i_frames <= 0:
+			set_modulate(Color(1, 1, 1))
+
 	move_and_slide()
 
 func free_state(delta):
@@ -50,61 +51,52 @@ func free_state(delta):
 	velocity = lerp(velocity, dir * speed, delta * accel)
 
 func dodge(delta):
-	# Set dash angle and start a timer
-	# On timeout, give iframes and start dashing; add another timer
-	# When THAT timer ends, return stop dashing and return to state 0
 	
-	match phase:
-		0:
-			dash_angle = dir * 2
-			timer = 1
-			phase = 1
-		1:
-			if timer > 0:
-				timer -= 1
-			else:
-				phase = 2
-		2:
-			i_frames = 60
-			timer = 20
-			dashing = true
-			phase = 3
-		3:
-			if timer > 0:
-				timer -= 1
-			else:
-				phase = 4
-		4:
-			state = 0
-			phase = 0
-			dashing = false
+	i_frames = 10
+	
+	if timer_on:
+		# Timer is on and dashing: so process the dash itself
+		if dashing:
+			velocity += (dash_angle * speed * 2 * delta)
 			
-	if dashing:
-		velocity += (dash_angle * speed * 2 * delta)	
+		# Timer is already processing, don't create a new one! So return
+		# Code will go back to where the timer was created when it times out
+		return
+	else:
+		if not dashing:
+			# Set dash angle and start a timer
+			dash_angle = dir * 2 # TODO: Why *2?
+			timer_on = true
+			await get_tree().create_timer(0.05).timeout
+			# On timeout, stop timer and start dashing
+			timer_on = false
+			dashing = true
+		else:
+			# Start dash
+			timer_on = true
+			await get_tree().create_timer(0.5).timeout
+			# Reset variables and return to free state on timeout
+			timer_on = false
+			dashing = false
+			state = 0
 
 func get_dir():
 	dir.x = Input.get_axis("move_left", "move_right")
 	dir.y = Input.get_axis("move_up", "move_down")
 	return dir.normalized()
 
-func damage_flash():
-	if not is_visible():
-		return
-	else:
-		hide()
-		await get_tree().create_timer(0.02).timeout # wait for 1 second
-		show()
-
 func _on_area_2d_body_entered(body):
 	if i_frames <= 0:
 		health -= 1
+		set_modulate(Color("red"))
 		hit.emit()
 		i_frames += 75
 	if health <= 0:
-		hide() # Player disappears after being hit.
+		hide()
 		hit.emit()
-		# Must be deferred as we can't change physics properties on a physics callback.
 		$CollisionShape2D.set_deferred("disabled", true) # Don't want player to get hit twiceace with function body.
+		# Must be deferred as we can't change physics properties on a physics callback.
+		await get_tree().create_timer(1).timeout
 		call_deferred("game_over")
 
 func game_over():
