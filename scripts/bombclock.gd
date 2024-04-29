90cc8eda35315fcbdd6389786e3353c518b470ed
MurlocFish20 <todian24@gmail.com>
2024-04-29 18:42:01 -0500

    explosion reduced, bomb animated

--- scripts/bombclock.gd
+++ scripts/bombclock.gd
@@ -6,8 +6,7 @@ extends CharacterBody2D
 
 var blowing_up = false
 var required_distance_for_boom = 180
-var number_of_blinks = 10
-var bomb_timer = .8
+var count_down = 5
 @export var explosion: PackedScene
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,30 +16,44 @@ func _process(delta):
 	else:
 		var player_distance = Vector2(position-player.position).length()
 		if ((player_distance >= 0 and player_distance < required_distance_for_boom) or (player_distance <= 0 and player_distance > (-1 * required_distance_for_boom))):
+			$AnimatedSprite2D.play("Alert Walk")
 			blowing_up = true
-			blow_up()
+			$Timer.start()
 		else:
 			normal_move(delta)
 
 
 func normal_move(delta):
+	$AnimatedSprite2D.play("Smile Walk")
 	velocity = Vector2.ZERO
 	velocity = position.direction_to(player.position) * speed
 	position += velocity * delta
 
 func blow_up():
-	var loop = 1
-	while (number_of_blinks > 0):
-		set_modulate(Color("red"))
-		await get_tree().create_timer(.4 - (.04 * loop)).timeout
-		set_modulate(Color(1, 1, 1, 1))
-		await get_tree().create_timer(bomb_timer).timeout
-		bomb_timer -= 0.08 * loop
-		if (bomb_timer <= 0):
-			bomb_timer = 0.01
-		loop += 1
-		number_of_blinks -= 1
 	var boom = explosion.instantiate()
 	boom.position = position
 	add_sibling(boom)
 	queue_free()
+
+
+func _on_timer_timeout():
+	if (count_down == 5):
+		$AnimatedSprite2D.play("5 Walk")
+		count_down -= 1
+	elif (count_down == 4):
+		$AnimatedSprite2D.play("4 Walk")
+		count_down -= 1
+	elif (count_down == 3):
+		$AnimatedSprite2D.play("3 Walk")
+		count_down -= 1
+	elif (count_down == 2):
+		$AnimatedSprite2D.play("2 Walk")
+		count_down -= 1
+	elif (count_down == 1):
+		$AnimatedSprite2D.play("1 Walk")
+		count_down -= 1
+		$Timer.one_shot = true
+	else:
+		$AnimatedSprite2D.play("Shock Walk")
+		await get_tree().create_timer(0.5).timeout
+		blow_up()
