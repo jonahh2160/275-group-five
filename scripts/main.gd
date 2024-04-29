f479101b791f99881dd7ef3d50ee0abb3eb2ab3c
MurlocFish20 <144245236+MurlocFish20@users.noreply.github.com>
2024-04-29 00:32:03 -0500

    Added waves
    If all enemies are dead will spawn n+1 enemies where n is the number of enemies from the prior wave with the first wave spawning 3 enemies

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,22 +5,33 @@ extends Node
 
 var mobSpawnerList
 var enemyList
+var wave = 0
+var base_num_enemies = 2
+var enemies_to_spawn
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mobSpawnerList = $MobSpawners.get_children()
-	enemyList = [wheelEnemy_scene]
-	spawn_enemy()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	enemyList = [bombEnemy_scene, wheelEnemy_scene]
+	start_next_wave()
 
 func spawn_enemy():
 	var enemyToSpawn = GlobalFunctions.getRandFromList(enemyList).instantiate()
 	var useSpawner = GlobalFunctions.getRandFromList(mobSpawnerList)
 	enemyToSpawn.position = useSpawner.position
-	useSpawner.add_sibling(enemyToSpawn)
+	useSpawner.get_parent().get_parent().get_node("Enemies").add_child(enemyToSpawn)
 
 func _on_player_hit():
 	$HUD.update_health(str($Player.health))
+
+func start_next_wave():
+	wave += 1
+	enemies_to_spawn = base_num_enemies + wave
+	while (enemies_to_spawn > 0):
+		await get_tree().create_timer(.3).timeout
+		spawn_enemy()
+		enemies_to_spawn -= 1
+
+
+func _on_check_for_enemies_timeout():
+	if (get_node("Enemies").get_child_count() == 0):
+		start_next_wave()
