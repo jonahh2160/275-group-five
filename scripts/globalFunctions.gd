1ff3d1491529354e2665d18c4509782115db0e4b
MurlocFish20 <todian24@gmail.com>
2024-05-02 21:33:24 -0500

    added settings menu

--- scripts/globalFunctions.gd
+++ scripts/globalFunctions.gd
@@ -1,9 +1,10 @@
 extends Node
 
-
+var enemy_spawn_sound
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+	enemy_spawn_sound = load("res://snd/metalpipe.mp3")
 
 # this is not new, this is the recommended way to get something similar to rand.choice()
 func getRandFromList(list):
